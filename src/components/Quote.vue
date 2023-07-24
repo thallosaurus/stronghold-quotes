88a956728bb60d4cbcea14a63e6f5b3294fd13da
Philipp Gaßner <activator112233@gmail.com>
2023-07-24 15:35:15 +0200

    first commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,42 @@
+<script>
+import q from '../assets/quotes.json'
+export default {
+    data() {
+        return {
+            quote: null
+        }
+    },
+    methods: {
+        changeQuote() {
+            let index = Math.floor(Math.random() * q.quotes.length);
+
+            let chosen = q.quotes[index];
+            // console.log(q, chosen);
+            this.quote = chosen;
+        },
+    },
+    mounted() {
+        // alert("mounted");
+        this.changeQuote();
+        // let index = Math.floor(Math.random() * q.quotes.length);
+
+        // let chosen = q.quotes[index];
+        // console.log(q, chosen);
+        // this.quote = chosen;
+    }
+
+}
+</script>
+
+<template>
+    <h1 id="quote" v-if="quote != null">
+        {{ quote }}
+    </h1>
+    <a href="#" @click="changeQuote">Neu</a>
+</template>
+
+<style>
+#quote {
+    /* border: 1px solid black; */
+}
+</style>
